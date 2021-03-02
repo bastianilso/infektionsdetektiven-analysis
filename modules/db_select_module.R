6d5f1d1ddc720f666282b8cdd874a157392e8674
Bastian Ilso <hougaard.junior@gmail.com>
2021-03-02 10:44:04 +0100

    db_select_module: Load all sessions
    In this case we want to do stats at an
    overall basis, so lets load in all
    sessions.

--- modules/db_select_module.R
+++ modules/db_select_module.R
@@ -21,12 +21,13 @@ db_select <- function(input, output, session, connected) {
   
   if (connected) {
     meta <- unique(RetreiveAllData("Meta"))
-    active_session = GetSessionID()
-    if (active_session == "NA") {
-      active_session <- as.character(meta$SessionID[1])
-      SetSessionID(active_session)
-    }
-    active_df = RetreiveCurrentData()
+    
+    #active_session = GetSessionID()
+    #if (active_session == "NA") {
+    #  active_session <- as.character(meta$SessionID[1])
+    #  SetSessionID(active_session)
+    #}
+    active_df <- RetreiveAllData("Full")
     current_trigger = current_trigger + 1
   }
 
@@ -36,41 +37,6 @@ db_select <- function(input, output, session, connected) {
     session = active_session,
     trigger = current_trigger
   )
-  
-  observeEvent(toReturn$df_meta, {
-    req(!is.null(toReturn$df_meta))
-    ids <- toReturn$df_meta$SessionID
-    timestamps <- toReturn$df_meta$Timestamp
-    emails <- toReturn$df_meta$Email
-    output$statusText <- renderText({ paste(length(ids), "sessions available.") })
-    output$sessionList <- renderUI({
-      lapply(1:length(ids), function(i) {
-        if (ids[i] == toReturn$session) {
-          tags$div(class = "font-weight-bold text-primary",
-             db_session_row_UI(ns(i))
-          )
-        } else {
-          db_session_row_UI(ns(i))
-        }
-      })
-    })
-    
-    chosen_row <- lapply(1:length(ids), function(i) {
-      callModule(db_session_row, i, meta$SessionID[i],meta$Email[i],meta$Timestamp[i])
-    })
-    
-    lapply(chosen_row, function(row) {
-      observeEvent(row$active, {
-        req(row$trigger > 0)
-        SetSessionID(row$sesid)
-        toReturn$df <<- RetreiveCurrentData()
-        toReturn$session <<- row$sesid
-        toReturn$trigger <<- toReturn$trigger + 1
-        
-      })
-    })
-    
-  })
   
   return(toReturn)
 }
