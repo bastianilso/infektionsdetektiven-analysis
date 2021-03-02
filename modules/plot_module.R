0f4a962f633489f8158f19ddd70d31a7d7047ba4
Bastian Ilso <hougaard.junior@gmail.com>
2021-03-02 11:10:38 +0100

    plot_module: adjust plot sizes

--- modules/plot_module.R
+++ modules/plot_module.R
@@ -11,7 +11,7 @@ plot_module_summary <- function(input, output, session, df) {
   vistemplate <- plot_ly() %>%
     config(scrollZoom = TRUE, displaylogo = FALSE, modeBarButtonsToRemove = c("pan2d","select2d","hoverCompareCartesian", "toggleSpikelines","zoom2d","toImage", "sendDataToCloud", "editInChartStudio", "lasso2d", "drawclosedpath", "drawopenpath", "drawline", "drawcircle", "eraseshape", "autoScale2d", "hoverClosestCartesian","toggleHover", "")) %>%
     layout(dragmode = "pan", showlegend=FALSE,
-           xaxis=list(mirror=T, showline=F,titlefont = list(size=16),tickfont = list(size=14), range=c(0,50)),
+           xaxis=list(mirror=T, showline=F,titlefont = list(size=16),tickfont = list(size=14), range=c(0,60)),
            yaxis=list(mirror=T, showline=F,titlefont = list(size=16),tickfont = list(size=14), range=c(0,100))
     )
   
@@ -53,8 +53,8 @@ plot_module_summary <- function(input, output, session, df) {
       add_trace(data=active_df %>% filter(Event.Event == "Sample", LevelNo == 7 ), x=~GameTime, y=~NumberOfInfected, type="scattergl", color=~paste(SessionID, LevelPlayID), mode="lines") %>%
       add_trace(data=active_df %>% filter(Event.Event == "Sample", LevelNo == 7 ), x=~GameTime, y=~(SubjectsOnStart-GameOverScore-NewInfectionSeconds), type="scattergl", color=I('red'), mode="lines") %>%
       layout(xaxis=list(title="Game Time, Lvl 7"), yaxis=list(title="Infection Spread"))
-    subplot(nrows = 2, titleX = TRUE, titleY = TRUE, margin = 0.08, fig1, fig2, fig3, fig4, fig5, fig6, fig7) %>%
-      layout(height = 700, widths=c(1,1,1,1,1,1,1))
+    subplot(nrows = 2, widths = c(0.22, 0.28,0.28, 0.22), titleX = TRUE, titleY = TRUE, margin = 0.04, fig1, fig2, fig3, fig4, fig5, fig6, fig7) %>%
+      layout(height = 700)
 
   })  
 }
